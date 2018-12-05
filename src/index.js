349b801b77dab2e8eba8c1139987f50bea9505d7
Fumi Kuranishi <kuranishi3221@gmail.com>
2018-12-05 18:08:08 +0100

    challenge2

--- src/index.js
+++ src/index.js
@@ -17,25 +17,60 @@ const propertyData = {
 function handleClick(e) {
   e.preventDefault();
   const mainEl = document.getElementById('main');
+  getData().then(function (value) {
+    mainEl.innerHTML = propertyData;
+  }).catch(function (error) {
+    mainEl.innerHTML = 'データの取得に失敗しました。';
+  }); 
   /* 
     getDataを呼び出して、mainEl.innerHTMLを利用して、結果を出力します。
+    getDataを呼び出す。
+    物件データが戻ったらそのデータを表示する。
+    エラーがあれば、エラーメッセージを表示する。
   */
 }
 
 function getData() {
+  fetchData().then(function (value) {
+    console.log(Promise.resolve);
+  }).catch(function (error) {
+    console.log(Promise.reject);
+  });  
   /* 
-    fetchDataを呼び出して、戻ってきたデータのsuccessの値を元にresolveで物件データまたは、rejectでエラーメッセージを返す。
+    fetchDataを呼び出して、戻ってきたデータのsuccessの値を元に
+    成功なら、Promise.resolveで物件データを返す。
+    失敗なら、Promise.rejectでエラーメッセージを返す。
   */
 }
 
 function fetchData() {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      const percentage = _.random(0,100);
+      if (percentage > 20) {
+        resolve({
+          success: true,
+          propertyData: propertyData
+        });
+      } else {
+        reject({
+          success: false,
+          message: 'データの取得に失敗しました。' 
+        });
+      }
+    }, 1000);
+  });
   /* 
-    lodashのrandom()を使って、80%の確率で正しいデータを返し、20%の確率でエラーを返すようにしましょう。
-    またsetTimeoutを利用して、1秒待ってから結果を得るようにします。
+    fetchDataはPromiseオブジェクトを返す。
+    setTimeoutを利用して、1秒間待ってから結果を返す。
+    lodashのrandom()を使って、80%の確率で正しいデータを返し、20%の確率でエラーが発生する。
+    成功の場合、{ success: true, propertyData: propertyData }が返る。
+    エラーの場合、{ success: false, message: 'データの取得に失敗しました。' }が返る。
   */
 }
 
 {
   const button1 = document.getElementById('button1');
   button1.addEventListener("click", handleClick);
+  console.log('ready');
 }
