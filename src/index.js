29f284c2927dfb2691f609720786ee49cbda0d7a
yuitamura <yuixtamura@gmail.com>
2019-04-08 11:41:57 +0200

    Updated function fetchData()

--- src/index.js
+++ src/index.js
@@ -29,6 +29,24 @@ function getData() {
 }
 
 function fetchData() {
+  /*
+  function random() {
+    let randomNum = _.random(1, 10);
+  }
+
+  return new Promise(
+    function(resolve, reject) { // Yui: resolve と rejectは onFulfilled と onRejected と同じ？
+     resolve(paramsOnFulfilled); {
+      success: true, propertyData: propertyData
+     }
+     reject(paramsOnRejected); {
+      success: false, message: 'データの取得に失敗しました。'
+     }
+     setTimeout(getData() { // Yui: ここのfunctionを何を使えばいいかわからない。
+     }, 1000);
+    }
+  );
+  */
   /* 
     lodashのrandom()を使って、80%の確率で正しいデータを返し、20%の確率でエラーを返すようにしましょう。
     またsetTimeoutを利用して、1秒待ってから結果を得るようにします。
