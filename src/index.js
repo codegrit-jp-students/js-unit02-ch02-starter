30150b231b3142c4ed360aa1eb8235b3f0d08f20
yuitamura <yuixtamura@gmail.com>
2019-04-23 10:47:15 +0200

    Updated function getData()

--- src/index.js
+++ src/index.js
@@ -17,49 +17,48 @@ const propertyData = {
 function handleClick(e) {
   e.preventDefault();
   const mainEl = document.getElementById('main');
+  mainEl.innerHTML = getData();
   /* 
     getDataを呼び出して、mainEl.innerHTMLを利用して、結果を出力します。
   */
 }
 
 function getData() {
+  fetchData().then(
+    (resolve) => { // not defined
+      console.log(propertyData);
+    },
+    (rejected) => { // not defined
+      console.log({message: 'データの取得に失敗しました。'});
+    }
+  );
   /* 
     fetchDataを呼び出して、戻ってきたデータのsuccessの値を元にresolveで物件データまたは、rejectでエラーメッセージを返す。
   */
 }
 
 function fetchData() {
-  const randomNum = _.random(1, 5);
-  if (randomNum <= 4) {
-    resolve(
-      Object.assign({
-        success: true,
-        propertyData: propertyData
-      }, propertyData)
-    );
-  } else {
-    reject({
-      success: false,
-      message: 'データの取得に失敗しました。'
-    });
-  };
-/*
-  return new Promise(
-    function(resolve, reject) { // Yui: resolve と rejectは onFulfilled と onRejected と同じ
-     resolve(); {
-     }
-     reject(); {
-     }
-     setTimeout(getData() { // Yui: ここのfunctionを何を使えばいいかわからない。
-     }, 1000);
-    }
-  );
-*/
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const randomNum = _.random(1, 5);
+      if (randomNum <= 4) {
+        resolve(
+          Object.assign({}, {success: true}, {propertyData})
+        );
+      } else {
+        reject({ // resolveでも検証する（全部関数ができてから）
+          success: false,
+          message: 'データの取得に失敗しました。'
+        });
+      }
+    }, 1000)
+  })
+};
+
   /* 
     lodashのrandom()を使って、80%の確率で正しいデータを返し、20%の確率でエラーを返すようにしましょう。
     またsetTimeoutを利用して、1秒待ってから結果を得るようにします。
   */ 
-}
 
 {
   const button1 = document.getElementById('button1');
